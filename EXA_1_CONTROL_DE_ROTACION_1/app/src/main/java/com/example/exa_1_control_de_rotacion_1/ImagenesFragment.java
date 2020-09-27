9cc0d9c2dbbbc03440ddf63580553eeb48362752
leslysifuentes <60679927+leslysifuentes@users.noreply.github.com>
2020-09-27 00:55:02 -0600

    EXA_1_CONTROL_DE_ROTACION_1

---
+++ EXA_1_CONTROL_DE_ROTACION_1/app/src/main/java/com/example/exa_1_control_de_rotacion_1/ImagenesFragment.java
@@ -0,0 +1,19 @@
+package com.example.exa_1_control_de_rotacion_1;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ImagenesFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_imagenes, container, false);
+    }
+}
---
+++ EXA_1_CONTROL_DE_ROTACION_1/app/src/main/java/com/example/exa_1_control_de_rotacion_1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.exa_1_control_de_rotacion_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
