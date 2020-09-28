c330e8d9fd4508aa1da2439b971442e22fff335f
leslysifuentes <60679927+leslysifuentes@users.noreply.github.com>
2020-09-28 08:26:00 -0600

    EXA_1_CONTROL_DE_ROTACION_2

---
+++ EXA_1_CONTROL_DE_ROTACION_2/app/src/main/java/com/example/exa_1_control_de_rotacion_2/tituloFragment.java
@@ -0,0 +1,64 @@
+package com.example.exa_1_control_de_rotacion_2;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link tituloFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class tituloFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public tituloFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment tituloFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static tituloFragment newInstance(String param1, String param2) {
+        tituloFragment fragment = new tituloFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_titulo, container, false);
+    }
+}
